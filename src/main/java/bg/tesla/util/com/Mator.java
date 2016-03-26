d7c25658495077b1931324165f8349fe16298b6b
achot <achot@desktop>
2016-03-26 08:01:25 +0200

    AOK 4
    Change-Id: I7f4fa53dfd5ffa8bf848b2b78501d797e1362fb0

--- src/main/java/bg/tesla/util/com/Mator.java
+++ src/main/java/bg/tesla/util/com/Mator.java
@@ -1,5 +1,23 @@
 package bg.tesla.util.com;
 
 public class Mator {
+	private String abd;
+	private String mcf;
+
+	public String getAbd() {
+		return abd;
+	}
+
+	public void setAbd(String abd) {
+		this.abd = abd;
+	}
+
+	public String getMcf() {
+		return mcf;
+	}
+
+	public void setMcf(String mcf) {
+		this.mcf = mcf;
+	}
 
 }
