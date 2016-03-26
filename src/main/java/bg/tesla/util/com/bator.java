73572c480c04526902bde45fd12fc92028c53ccf
achot <achot@desktop>
2016-03-26 07:55:18 +0200

    AOK 3
    Change-Id: Ic5763e75bf932f1836617c46542860637f392600

--- src/main/java/bg/tesla/util/com/bator.java
+++ src/main/java/bg/tesla/util/com/bator.java
@@ -1,5 +1,23 @@
 package bg.tesla.util.com;
 
 public class bator {
+	private String sdf;
+	private String mdf;
+
+	public String getSdf() {
+		return sdf;
+	}
+
+	public void setSdf(String sdf) {
+		this.sdf = sdf;
+	}
+
+	public String getMdf() {
+		return mdf;
+	}
+
+	public void setMdf(String mdf) {
+		this.mdf = mdf;
+	}
 
 }
